6b0389d34b078c6dc36eff6201e2ca784f4b432f
czxieddan <czxieddan@gmail.com>
2026-09-15 22:21:51 +0800

    chore: 将 ASPF 更名为 PS

--- common/defines/ASPF_career_profile.lua
+++
@@ -1,2 +0,0 @@
-NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP = "ASPF"
-NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP_NAME = "ASPF"
---
+++ common/defines/PS_career_profile.lua
@@ -0,0 +1,2 @@
+NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP = "PS"
+NDefines_CareerProfile.NCareerProfile.MOD_STATISTICS_GROUP_NAME = "PS"
